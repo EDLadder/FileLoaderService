a5c09d7bd160535444577918cef988629798eb60
EDLadder <dascaleugen@outlook.com>
2022-08-15 23:19:25 +0300

    add app config

---
+++ src/service-app/Data/DataContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using service_app.Models;
+
+namespace service_app.Data
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to postgres with connection string from app settings
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        // accessing and managing serverfile from db
+        public DbSet<ServerFile> ServerFiles { get; set; }
+    }
+}
+
